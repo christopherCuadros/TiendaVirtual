386a2a742fe240281ae2f8e97b8697463fb7c371
christopher-cuadros <110701105+visual-junior@users.noreply.github.com>
2023-12-04 15:37:21 -0500

    test3

--- src/layout/ClientLayout.jsx
+++ src/layout/ClientLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../components/Footer";
+<<<<<<< HEAD
 import StickyNavbar from "../components/StickyNavbar";
 
 const ClientLayout = () => {
@@ -9,6 +10,16 @@ const ClientLayout = () => {
                 <div>
                     <Outlet/>
                 </div>
+=======
+import Pago from "../pages/pago";
+const ClientLayout = () => {
+    return(
+        <>
+            <Navbar/>
+                <div>client layout</div>
+
+                <Pago/>
+>>>>>>> f27664c (avance)
             <Footer/>
         </>
         
